de2e7ec30139e33854d660abcef68bff18ba787f
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2019-01-01 18:16:34 +0000

    Minor progress

--- Assets/Scripts/Battle/BattleConfig.cs
+++ Assets/Scripts/Battle/BattleConfig.cs
@@ -4,7 +4,5 @@ public struct BattleConfig
 {
     public BattleCharacter[] playerCharacters;
     public BattleCharacter[] enemyCharacters;
-    public Transform[] playerLocations;
-    public Transform[] enemyLocations;
     public IBattleManagerState initialState;
 }
--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -6,29 +6,39 @@ public class BattleManager : MonoBehaviour
     Stack<IBattleManagerState> states;
     List<BattleCharacter> battleCharacters;
     Queue<BattleAction> battleActions;
+    [SerializeField]
+    Transform[] playerSpawnPoints;
+    [SerializeField]
+    Transform[] enemySpawnPoints;
 
     public List<BattleCharacter> GetCharacters
     {
         get { return battleCharacters; }
     }
-    
+
     void Update()
     {
         states.Peek().Update();
     }
 
-    public void Init(BattleConfig config) // TODO: should take a battleConfig object as an argument to init everything
+    public void Init(BattleConfig config)
     {
         battleActions = new Queue<BattleAction>();
         battleCharacters = new List<BattleCharacter>();
         states = new Stack<IBattleManagerState>();
 
+        for (int i = 0; i < config.playerCharacters.Length; i++)
+        {
+            // spawn characters at appropriate positions
+        }
+
+        for (int i = 0; i < config.enemyCharacters.Length; i++)
+        {
+            // spawn enemies at appropriate positions
+        }
+
         states.Push(config.initialState ?? new IntroBattleManagerState());
         states.Peek().OnEnter(this);
-
-        // battle scene should be a level with this script on which loads the
-        // appropriate actors based on the config object passed to it
-        // as well as what state the battle should start in ie. back attack, first strike etc
     }
 
     public void ChangeState(IBattleManagerState newState)
