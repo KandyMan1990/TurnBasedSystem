075d766e4059080be28d7353b47c9aea6c9cd36f
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2019-01-16 19:28:28 +0000

    Created base item class, recovery item class, example items and temp image

---
+++ Assets/Scripts/Inventory/BaseItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class BaseItem : ScriptableObject
+{
+    [SerializeField]
+    string description;
+    [SerializeField]
+    int cost;
+    [SerializeField]
+    Sprite icon;
+
+    public string Description { get => description; }
+    public int Cost { get => cost; }
+    public Sprite Icon { get => icon; }
+
+    public abstract bool Use();
+}
---
+++ Assets/Scripts/Inventory/RecoveryItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Recovery Item", menuName = "RPG/Item/Recovery Item")]
+public class RecoveryItem : BaseItem
+{
+    [SerializeField]
+    int healAmountValue;
+    [SerializeField]
+    [Range(0f, 1f)]
+    float healAmountPercent;
+    [SerializeField]
+    bool partyItem;
+    // [SerializeField]
+    // BaseStatus[] statusToRemove;
+
+    public override bool Use()
+    {
+        throw new System.NotImplementedException();
+    }
+}
