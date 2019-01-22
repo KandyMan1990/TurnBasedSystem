7ca264aa4fc886e390616c18665225854f8c5890
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2019-01-22 19:47:21 +0000

    Added inventory restriction mask

---
+++ Assets/Scripts/Inventory/InventoryRestrictionMask.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[Flags]
+public enum InventoryRestrictionMask : byte
+{
+    UNKNOWN = 0,
+    SHOW_IN_ITEM_UNUSABLE = 1,
+    SHOW_IN_ITEM_USABLE = 2,
+    SHOW_EVERYWHERE_USABLE = 4
+}
